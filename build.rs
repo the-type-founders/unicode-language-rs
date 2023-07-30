e9ed55f75ab617600539c31162475f3c5e3916e9
Bram Stein <b.l.stein@gmail.com>
2023-07-30 23:35:23 +0800

    Initial pass at a Rust port.

---
+++ build.rs
@@ -0,0 +1,212 @@
+// build.rs
+
+use glob::glob;
+use serde::{de::Error, Deserialize, Deserializer};
+use serde_yaml::{self};
+use std::env;
+use std::fs::read_to_string;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+#[derive(Debug, PartialEq, Clone)]
+struct Codepoint(i32, i32);
+
+impl<'de> Deserialize<'de> for Codepoint {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s: &str = Deserialize::deserialize(deserializer)?;
+
+        if s.contains("..") {
+            s.split("..")
+                .map(|x| x.parse::<i32>())
+                .collect::<Result<Vec<_>, _>>()
+                .map(|v| Codepoint(v[0], v[1]))
+                .map_err(D::Error::custom)
+        } else {
+            s.parse::<i32>()
+                .map(|i| Codepoint(i, i))
+                .map_err(D::Error::custom)
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct Language {
+    //    version: String,
+    anglicized_name: String,
+    native_name: String,
+    codepoints: Vec<Codepoint>,
+    code: Option<String>,
+}
+
+fn parse_yaml<T: AsRef<Path>>(path: T) -> Result<Language, String> {
+    let path = path.as_ref();
+
+    let s = read_to_string(path).expect("Could not open file.");
+
+    // The Serde YAML parser expects YAML types to have names that are valid
+    // Rust identifiers. Sadly, that is not the case here, so we manually perform
+    // a string replace to patch up the data.
+    let mut d: Language = serde_yaml::from_str(&s.replace("ruby/range", "Range"))
+        .expect("Failed to parse YAML file.");
+
+    d.code = Some(
+        path.file_name()
+            .expect("Could not get filename")
+            .to_os_string()
+            .into_string()
+            .unwrap(),
+    );
+
+    // Sort the codepoints so we can exit early when running the detection code.
+    d.codepoints.sort_by_key(|c| c.0);
+
+    Ok(d)
+}
+
+#[derive(Debug)]
+pub struct Metadata {
+    pub code: String,
+    pub name: String,
+    pub native_name: String,
+}
+
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("data.rs");
+    let mut f = File::create(dest_path).unwrap();
+
+    let languages: Vec<Language> = glob("./speakeasy/data/*")
+        .expect("Failed to read the speakeasy/data directory.")
+        .map(|path| path.expect("File does not exist."))
+        .map(parse_yaml)
+        .map(|language| language.expect("Failed to parse language data."))
+        .collect();
+
+    let ranges: Vec<Vec<Codepoint>> = languages.iter().map(|l| l.codepoints.to_vec()).collect();
+    let totals: Vec<i32> = ranges
+        .iter()
+        .map(|codepoints| codepoints.iter().map(|c| c.1 - c.0 + 1).sum::<i32>())
+        .collect();
+
+    let metadata: Vec<Metadata> = languages
+        .iter()
+        .map(|l| Metadata {
+            code: l.code.as_ref().unwrap().clone(),
+            name: l.anglicized_name.clone(),
+            native_name: l.native_name.clone(),
+        })
+        .collect();
+
+    let length = ranges.len();
+
+    let ranges_str = ranges
+        .iter()
+        .map(|codepoints| {
+            format!(
+                "vec![{}]",
+                codepoints
+                    .iter()
+                    .map(|c| format!("({},{})", c.0, c.1))
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            )
+        })
+        .collect::<Vec<_>>()
+        .join(", ");
+
+    let totals_str = format!("{:?}", totals);
+    let metadata_str = format!("{:?}", metadata);
+
+    write!(
+        f,
+        "
+use std::sync::OnceLock;
+
+struct Metadata {{
+    code: &'static str,
+    name: &'static str,
+    native_name: &'static str,
+}}
+
+#[cfg(not(test))]
+fn ranges() -> &'static [Vec<(i32,i32)>; {}] {{
+  static RANGES: OnceLock<[Vec<(i32, i32)>; {}]> = OnceLock::new();
+
+  RANGES.get_or_init(|| {{
+    [{}]
+  }})
+}}
+
+#[cfg(test)]
+fn ranges() -> &'static [Vec<(i32,i32)>; 4] {{
+  static RANGES: OnceLock<[Vec<(i32, i32)>; 4]> = OnceLock::new();
+
+  RANGES.get_or_init(|| {{
+    [vec![(1, 3)], vec![(4, 6)], vec![(7, 9)], vec![(8, 8)]]
+  }})
+}}
+
+#[cfg(not(test))]
+fn totals() -> &'static [i32; {}] {{
+  static TOTALS: OnceLock<[i32; {}]> = OnceLock::new();
+
+  TOTALS.get_or_init(|| {{
+    {}
+  }})
+}}
+
+#[cfg(test)]
+fn totals() -> &'static [i32; 4] {{
+  static TOTALS: OnceLock<[i32; 4]> = OnceLock::new();
+
+  TOTALS.get_or_init(|| {{
+    [3, 3, 3, 1]
+  }})
+}}
+
+#[cfg(not(test))]
+fn metadata() -> &'static [Metadata; {}] {{
+  static METADATA: OnceLock<[Metadata; {}]> = OnceLock::new();
+
+  METADATA.get_or_init(|| {{
+    {}
+  }})
+}}
+
+#[cfg(test)]
+fn metadata() -> &'static [Metadata; 4] {{
+  static METADATA: OnceLock<[Metadata; 4]> = OnceLock::new();
+
+  METADATA.get_or_init(|| {{
+    [
+      Metadata {{ code: \"t1\", name: \"test1\", native_name: \"ntest1\" }},
+      Metadata {{ code: \"t2\", name: \"test2\", native_name: \"ntest2\" }},
+      Metadata {{ code: \"t3\", name: \"test3\", native_name: \"ntest3\" }},
+      Metadata {{ code: \"t4\", name: \"test4\", native_name: \"ntest4\" }},
+    ]
+  }})
+}}
+
+#[cfg(not(test))]
+const SIZE: usize = {};
+
+#[cfg(test)]
+const SIZE: usize = 4;
+",
+        length,
+        length,
+        ranges_str,
+        length,
+        length,
+        totals_str,
+        length,
+        length,
+        metadata_str,
+        length
+    )
+    .expect("Failed to write data file.");
+}
---
+++ src/lib.rs
@@ -0,0 +1,120 @@
+include!(concat!(env!("OUT_DIR"), "/data.rs"));
+
+#[derive(Debug)]
+pub struct Match {
+    pub code: String,   // ISO 639-1 language codes;
+    pub name: String,   // English name;
+    pub native: String, // Name in native script;
+    pub count: i32,     // The number of codepoints matched;
+    pub score: f32, // The score (number of codepoints match divided by the total for the language).
+}
+
+pub fn detect<T>(codepoints: T, threshold: f32) -> Vec<Match>
+where
+    T: IntoIterator<Item = i32>,
+{
+    let mut counts = [0; SIZE];
+    let ranges = ranges();
+
+    for value in codepoints {
+        for i in 0..SIZE {
+            for k in 0..ranges[i].len() {
+                if value >= ranges[i][k].0 && value <= ranges[i][k].1 {
+                    counts[i] += 1;
+                }
+
+                if ranges[i][k].0 > value {
+                    break;
+                }
+            }
+        }
+    }
+
+    let totals = totals();
+    let metadata = metadata();
+    let mut result: Vec<Match> = Vec::new();
+
+    for i in 0..SIZE {
+        let score = counts[i] as f32 / totals[i] as f32;
+        if score >= threshold && counts[i] > 0 {
+            result.push(Match {
+                code: metadata[i].code.to_string(),
+                name: metadata[i].name.to_string(),
+                native: metadata[i].native_name.to_string(),
+                count: counts[i],
+                score,
+            });
+        }
+    }
+
+    result.sort_by(|a, b| a.score.partial_cmp(&b.score).unwrap());
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_returns_an_empty_array() {
+        let result = detect([], 0.5);
+        assert_eq!(result.len(), 0);
+    }
+
+    #[test]
+    fn it_returns_an_empty_array_with_an_invalid_codepoint() {
+        let result = detect([256], 0.5);
+        assert_eq!(result.len(), 0);
+    }
+
+    #[test]
+    fn it_returns_the_test_language() {
+        let result = detect([1], 0.0);
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].code, "t1");
+        assert_eq!(result[0].name, "test1")
+    }
+
+    #[test]
+    fn it_does_not_return_if_threshold_not_met() {
+        let result = detect([1, 2], 1.0);
+        assert_eq!(result.len(), 0);
+    }
+
+    #[test]
+    fn it_returns_if_threshold_is_met() {
+        let result = detect([1, 2, 3], 1.0);
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].code, "t1");
+        assert_eq!(result[0].name, "test1");
+    }
+
+    #[test]
+    fn it_returns_if_threshold_is_partially_met() {
+        let result = detect([1, 2], 0.6);
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].code, "t1");
+        assert_eq!(result[0].name, "test1");
+    }
+
+    #[test]
+    fn it_returns_multiple_languages() {
+        let result = detect([1, 4], 0.0);
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0].code, "t1");
+        assert_eq!(result[0].name, "test1");
+        assert_eq!(result[1].code, "t2");
+        assert_eq!(result[1].name, "test2");
+    }
+
+    #[test]
+    fn it_returns_overlapping_languages() {
+        let result = detect([8], 0.0);
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0].code, "t3");
+        assert_eq!(result[0].name, "test3");
+        assert_eq!(result[1].code, "t4");
+        assert_eq!(result[1].name, "test4");
+    }
+}
